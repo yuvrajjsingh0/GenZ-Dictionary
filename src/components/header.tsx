a1f53dc8f04767ed71e62ae8311825a7b7c0694f
Yuvraj Singh <yuvrajjsingh0@gmail.com>
2024-03-08 07:02:42 +0530

    Added Discord and Twitter links in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,32 @@
+import Link from 'next/link';
 import React from 'react'
+import { FaDiscord } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 export const Header = () => {
+    const discordMessages = Math.ceil(Math.random()*10);
     return (
         <div className="navbar bg-base-100 sticky top-0 z-50">
-            <a className="btn btn-ghost text-xl">GenZ Dictionary</a>
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">GenZ Dictionary</a>
+            </div>
+            <div className="flex-none">
+                <Link href={"https://discord.gg/E7vZarD8j8"} target='_blank'>
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle mx-4">
+                        <div className="indicator">
+                            <FaDiscord size={"2em"}/>
+                            <span className="badge badge-sm indicator-item">{discordMessages}</span>
+                        </div>
+                    </div>
+                </Link>
+                
+                <Link href={"https://x.com/its_yuvrajsingh"} target='_blank'>
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle mx-4 ">
+                        <FaXTwitter size={"2em"}/>
+                    </div>
+                </Link>
+                
+            </div>
         </div>
     )
 }
