9388b103d78b29c188d12ce1350778c8882301c2
Yuvraj Singh <yuvrajjsingh0@gmail.com>
2024-04-21 01:25:56 +0530

    Added google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="cupcake" style={{scrollBehavior:'smooth'}}>
+      <GoogleTagManager gtmId="GTM-NXM3QLTX" />
       <body className={inter.className}>{children}</body>
     </html>
   );
